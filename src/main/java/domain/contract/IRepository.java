a975b62be7fff0d95f0f6f9420eacccc70ff5c89
maruki00 <aitbaha48@gmail.com>
2026-03-11 01:43:27 +0300

    Update IRepository.java

--- src/main/java/domain/contract/IRepository.java
+++ src/main/java/domain/contract/IRepository.java
@@ -1,5 +1,12 @@
 package domain.contract;
 
-public interface IRepository {
+import java.util.List;
+import java.util.Optional;
 
+public interface IRepository {
+public void add(IEvent event) ;
+    public boolean remove(int id) ;
+    public void clear() ;
+    public Optional<IEvent> searchById(int id) ;
+    public List<IEvent> searchByStr(String query);
 } 
