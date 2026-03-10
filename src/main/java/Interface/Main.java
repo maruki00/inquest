40d085666cd23fef370fdf60f3e35b82d698fe5d
maruki00 <aitbaha48@gmail.com>
2026-03-11 01:43:27 +0300

    Update Main.java

--- src/main/java/Interface/Main.java
+++ src/main/java/Interface/Main.java
@@ -1,8 +1,18 @@
 package Interface;
 
+
+import app.Services.HttpdAccessLogServcie;
+import infra.repository.InMemory;
+
 public class Main {
         public static void main(String[] args) {
-                System.out.println("hello");
+                var rep = new InMemory();
+                var srv = new HttpdAccessLogServcie(rep);
+                //InputStream is = Main.class.getClassLoader().getResourceAsStream("access.log");
+                srv.Parse("/home/user/dev/lab/src/main/java/Interface/access.log");
+                for(var x : rep.events()){
+                        System.out.printf("event: %s\n", x.srcIp());
+                }
         }
 }
 
