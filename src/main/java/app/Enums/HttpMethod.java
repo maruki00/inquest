b9cb8c42dea3d3e1fefc073d972d5462203b8872
maruki00 <aitbaha48@gmail.com>
2026-03-11 01:43:27 +0300

    Add HttpMethod.java

---
+++ src/main/java/app/Enums/HttpMethod.java
@@ -0,0 +1,27 @@
+package app.Enums;
+
+public enum HttpMethod {
+
+    GET("GET"),
+    POST("POST"),
+    PUT("PUT"),
+    DELETE("DELETE"),
+    PATCH("PATCH"),
+    HEAD("HEAD"),
+    OPTIONS("OPTIONS");
+
+    private final String value;
+
+    HttpMethod(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
