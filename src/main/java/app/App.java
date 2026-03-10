a432f87d51c4920a7b26d3874a71e0f214a03035
maruki00 <aitbaha48@gmail.com>
2026-03-11 01:43:27 +0300

    Update App.java

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,5 +1,14 @@
 package app;
 
+
+import domain.contract.IRepository;
+import domain.contract.ParserService;
+
 public class App {
-    
+    public IRepository   repository;
+    public ParserService service;
+    public App(IRepository repository, ParserService service){
+        this.repository = repository;
+        this.service = service;
+    } 
 }
