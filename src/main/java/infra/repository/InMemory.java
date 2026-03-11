9769c0f33c275edfaf20eae00cdbb2871634315d
maruki00 <aitbaha48@gmail.com>
2026-03-11 19:28:49 +0300

    Update InMemory.java

--- src/main/java/infra/repository/InMemory.java
+++ src/main/java/infra/repository/InMemory.java
@@ -1,12 +1,8 @@
 package infra.repository;
-
-
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
-
 import domain.contract.IEvent;
 import domain.contract.IRepository;
-
 public class InMemory implements IRepository {
     private final List<IEvent> events = new CopyOnWriteArrayList<>();
 
@@ -33,9 +29,19 @@ public class InMemory implements IRepository {
                 .findFirst();
     }
 
-    public List<IEvent> searchByStr(String query) {
+    public List<IEvent> searchByIP(String query) {
         if (query == null) return List.of();
 
+        return events.stream()
+                .filter(e -> 
+                    query.equalsIgnoreCase(e.srcIp()) || 
+                    query.equalsIgnoreCase(e.dstIp())
+                )
+                .toList(); 
+    }
+
+    public List<IEvent> searchByStr(String query) {
+        if (query == null) return List.of();
         return events.stream()
                 .filter(e -> 
                     query.equalsIgnoreCase(e.srcIp()) || 
