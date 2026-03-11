673ec7dfcbf5de9362313400b529311b44238a52
maruki00 <aitbaha48@gmail.com>
2026-03-11 19:28:49 +0300

    Update Main.java

--- src/main/java/Interface/Main.java
+++ src/main/java/Interface/Main.java
@@ -1,20 +1,26 @@
 package Interface;
 
 
+import java.util.List;
+
 import app.Services.HttpdAccessLogServcie;
 import infra.repository.InMemory;
-import infra.templates.ddos;
+import infra.templates.DDoS;
+import infra.templates.Injection;
 
 public class Main {
         public static void main(String[] args) {
                 var rep = new InMemory();
                 var srv = new HttpdAccessLogServcie(rep);
-                //InputStream is = Main.class.getClassLoader().getResourceAsStream("access.log");
                 srv.Parse("/home/user/dev/lab/src/main/java/Interface/access.log");
-                // for(var x : rep.datasource()){
-                //         System.out.printf("event: %s\n", x.toString());
-                // }
-                srv.Analyse(new ddos());
+                var templates =  List.of(
+                        new DDoS(),
+                        new Injection()
+                );
+                for (var obj : templates){
+                        srv.Analyse(obj);
+                }
+             //  srv.Analyse(new Injection());
         }
 }
 
