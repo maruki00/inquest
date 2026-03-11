c80fcb82da4f45f00f1687b2141680c33e1ab3af
maruki00 <aitbaha48@gmail.com>
2026-03-11 20:30:42 +0300

    Update Basic.java

--- src/main/java/infra/templates/Basic.java
+++ src/main/java/infra/templates/Basic.java
@@ -1,20 +1,83 @@
 package infra.templates;
 
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import domain.contract.IEvent;
-
-public class Basic {
-    // TODO: implementation
+import domain.contract.Itemplete;
+/**
+ done:* Line 1: From which IP do requests occur most frequently? (Identify which other property occurs most frequently and least frequently in your specific log format).
+ done:* Line 2: Which HTTP method is called most often? (Which command or set of commands appears most frequently in the logs?)
+ done:* Line 3: Which URI is requested most frequently?
+ done* Line 4: On which day were there the most requests?
+ done* Line 5: What time period do the logs cover?
+ * Line 6: Suspicious requests (multiple identical requests in a row, multiple requests that received an error status code, etc.—feel free to use your imagination here).
+ * Line 7: Track any number of events of the same type/code from a single key within a short time window.
+*/
+public class Basic implements Itemplete{
+    
     public String scan(List<IEvent> events) {
-        List<IEvent> sortedEvents = events.stream().sorted(Comparator.comparing(IEvent::timestamp)).toList();
-        if (!sortedEvents.isEmpty()) {
-            IEvent first = sortedEvents.getFirst();
-            IEvent last = sortedEvents.getLast();
-            System.out.println(first.timestamp());
-            System.out.println(last.timestamp());
+        var mostFrequentIP = events.stream().
+                                  collect(Collectors.groupingBy(IEvent::srcIp, Collectors.counting())).
+                                  entrySet().
+                                  stream().
+                                  max(Map.Entry.comparingByValue());
+        
+        var mostFrequentHTTPMethod = events.stream().
+                                  collect(Collectors.groupingBy(IEvent::message, Collectors.counting())).
+                                  entrySet().
+                                  stream().
+                                  max(Map.Entry.comparingByValue());
+       
+        var mostFrequentURI = events.stream().
+                                  collect(Collectors.groupingBy(IEvent::subject, Collectors.counting())).
+                                  entrySet().
+                                  stream().
+                                  max(Map.Entry.comparingByValue());
+
+        var mostFrequentRequestsInADay = events.stream().
+                                  collect(Collectors.groupingBy(IEvent::timestamp, Collectors.counting())).
+                                  entrySet().
+                                  stream().
+                                  max(Map.Entry.comparingByValue());
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("\n   ========[ Basic Template ]==========\n");
+        builder.append("----------------------------------------------\n");
+        if(mostFrequentIP.isPresent()){
+            builder.append(String.format("Top Attacker IP  : %-15s (%5d hits)\n", 
+                                            mostFrequentIP.get().getKey(), 
+                                            mostFrequentIP.get().getValue() ) 
+            );
         }
-        return "";
+
+        if(mostFrequentHTTPMethod.isPresent()){
+            builder.append(String.format("Dominant Method  : %-15s (%5d hits)\n", 
+                                            mostFrequentHTTPMethod.get().getKey(), 
+                                            mostFrequentHTTPMethod.get().getValue())
+            );
+        }
+
+        if(mostFrequentURI.isPresent()){
+            builder.append(String.format("Hot Resource     : %-15s (%5d hits)\n", 
+                                            mostFrequentURI.get().getKey(), 
+                                            mostFrequentURI.get().getValue())
+            );
+        }
+        if(mostFrequentRequestsInADay.isPresent()){
+            builder.append(String.format("Peak Traffic Day : %-15s\n", 
+                                                mostFrequentRequestsInADay.get().getKey()
+                                        )
+            );
+        }
+
+
+        //builder.append(String.format("Potential Injecation : %-15s ", suspIps.get()));
+        return builder.toString();
     }
 }
