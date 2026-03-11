e3b6830dc1f2d7a45b7b3f1b989b79d548d506bf
maruki00 <aitbaha48@gmail.com>
2026-03-11 19:28:49 +0300

    Add DDoS.java

---
+++ src/main/java/infra/templates/DDoS.java
@@ -0,0 +1,31 @@
+package infra.templates;
+import java.util.List;
+import java.util.stream.Collectors;
+import domain.contract.IEvent;
+import domain.contract.Itemplete;
+
+public class DDoS implements Itemplete {
+    private static final Integer MAX_ALLOWED_REQUESTS = 1000;
+
+    public String scan(List<IEvent> events) {
+
+        var suspIps = events.stream().
+                                  collect(Collectors.groupingBy(IEvent::srcIp, Collectors.counting())).
+                                  entrySet().
+                                  stream().
+                                  filter(e -> e.getValue() > MAX_ALLOWED_REQUESTS).
+                                  collect(Collectors.toList());
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("   ========[ DDOS Template ]============\n");
+        builder.append("------------------------------------------\n");
+        if (suspIps.isEmpty()) {
+            builder.append("ddos not detected");
+        }else{
+            for (var x : suspIps){
+                builder.append(String.format("Potential DDoS : %-15s | Times : %-5d%n", x.getKey(), x.getValue()));
+            }
+        }
+        return builder.toString();
+    }
+}
