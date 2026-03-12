b2d0966ab07287e8ebb80fc429eddfb9e415684f
maruki00 <aitbaha48@gmail.com>
2026-03-12 19:40:30 +0300

    Update DDoS.java

--- src/main/java/infra/templates/DDoS.java
+++ src/main/java/infra/templates/DDoS.java
@@ -17,10 +17,10 @@ public class DDoS implements Itemplete {
                                   collect(Collectors.toList());
 
         StringBuilder builder = new StringBuilder("\033[31m");
-        builder.append("   ========[ DDOS Template ]============\n");
+        builder.append("   ========[ DDoS Template ]============\n");
         builder.append("------------------------------------------\n");
         if (suspIps.isEmpty()) {
-            builder.append("ddos not detected");
+            builder.append("No suspicious activity detected.");
         }else{
             for (var x : suspIps){
                 builder.append(String.format("Potential DDoS : %-15s | Times : %-5d%n", x.getKey(), x.getValue()));
