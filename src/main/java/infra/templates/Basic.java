85be7197d49dbcbbd8a2993349172f9a30fb7ccb
maruki00 <aitbaha48@gmail.com>
2026-03-12 23:22:29 +0300

    Update Basic.java

--- src/main/java/infra/templates/Basic.java
+++ src/main/java/infra/templates/Basic.java
@@ -8,15 +8,8 @@ import domain.contract.IEvent;
 import domain.contract.Itemplete;
 import io.github.cdimascio.dotenv.Dotenv;
 
-/**
- done:* Line 1: From which IP do requests occur most frequently? (Identify which other property occurs most frequently and least frequently in your specific log format).
- done:* Line 2: Which HTTP method is called most often? (Which command or set of commands appears most frequently in the logs?)
- done:* Line 3: Which URI is requested most frequently?
- done* Line 4: On which day were there the most requests?
- done* Line 5: What time period do the logs cover?
- done* Line 6: Suspicious requests (multiple identical requests in a row, multiple requests that received an error status code, etc.—feel free to use your imagination here).
- * Line 7: Track any number of events of the same type/code from a single key within a short time window.
-*/
+
+
 
 public class Basic implements Itemplete{
     
@@ -46,7 +39,7 @@ public class Basic implements Itemplete{
                                   max(Map.Entry.comparingByValue());
 
         StringBuilder builder = new StringBuilder("\033[0;34m");
-        builder.append("\n   ========[ Basic Template ]==========\n");
+        builder.append("==========[     Basic Template     ]==========\n");
         builder.append("----------------------------------------------\n");
         if(mostFrequentIP.isPresent()){
             builder.append(String.format("Top Attacker IP  : %-15s (%5d hits)\n", 
