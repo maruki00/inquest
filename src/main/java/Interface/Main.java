a444b632b70ef534885fd5c4317100c796eb7878
maruki00 <aitbaha48@gmail.com>
2026-03-12 19:40:30 +0300

    Update Main.java

--- src/main/java/Interface/Main.java
+++ src/main/java/Interface/Main.java
@@ -6,18 +6,21 @@ import java.util.List;
 import app.Services.HttpdAccessLogServcie;
 import infra.repository.InMemory;
 import infra.templates.Basic;
+import infra.templates.BruteForce;
 import infra.templates.DDoS;
 import infra.templates.Injection;
-
+import io.github.cdimascio.dotenv.Dotenv;
 public class Main {
         public static void main(String[] args) {
+                Dotenv dotenv = Dotenv.load();
                 var rep = new InMemory();
                 var srv = new HttpdAccessLogServcie(rep);
                 srv.Parse(ClassLoader.getSystemResource("access.log").getPath());
                 var templates =  List.of(
                         new Basic(),
-                        new DDoS(),
-                        new Injection()
+                        new BruteForce(),
+                        new Injection(),
+                        new DDoS()
                 );
                 for (var obj : templates){
                         srv.Analyse(obj);
